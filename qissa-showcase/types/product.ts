6dd433c4c4d0943dad804b5e6dd5eaaf6b77e6b4
abukarshamjad2006-source <abukarsh.amjad2006@gmail.com>
2026-08-11 10:39:21 +0300

    Update product.ts

--- qissa-showcase/types/product.ts
+++ qissa-showcase/types/product.ts
@@ -2,19 +2,22 @@ export type ColorId = "black" | "beige" | "white" | "navy";
 
 export interface ProductColor {
   id: ColorId;
-  /** Arabic display label for the color */
   label: string;
-  /** Swatch hex used for the selector UI */
   swatch: string;
-  /** Folder name under /public/products/<folder> */
   folder: string;
-  /** Number of full-bleed scroll images for this color (1.jpg ... N.jpg) */
   imageCount: number;
 }
 
 export interface FabricDetail {
   label: string;
   value: string;
+}
+
+export interface SizeRow {
+  heightRange: string;
+  weightRange: string;
+  relaxedFit: string;
+  oversizedFit: string;
 }
 
 export interface ProductContent {
@@ -28,4 +31,7 @@ export interface ProductContent {
   colors: ProductColor[];
   nextButtonLabel: string;
   backButtonLabel: string;
+  sizeChartTitle: string;
+  sizeChartIntro: string;
+  sizeChartRows: SizeRow[];
 }
